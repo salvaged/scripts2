67eb6371062ea1d5791b321e315ff72577bcce13
Adrian Tedesco <salvaged@gmail.com>
2022-03-30 12:02:52 -0400

    Update rfa.sh

--- rfa.sh
+++ rfa.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#Purpose is to remove the Messages from iMessage on your mac and remove attachments, can be run using a cronjob
 
 killall Messages
 echo "$USER"
 cd /Users/$USER/Library/Messages/Attachments/
 rm -rf *
+cd /Users/$USER/Library/Messages/
+rm -rf chat.db
+pause 5
 open -a Messages.app
 exit
